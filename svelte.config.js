a2c25b5816aca36a82ce573ddf765b6eb183cb31
Levi Tan Ong <levi.ong@gmail.com>
2024-10-24 23:06:23 +0800

    github workflow for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,11 +9,20 @@ const config = {
     preprocess: [vitePreprocess(), mdsvex()],
 
     kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        }
+    },
 
     extensions: [".svelte", ".svx"]
 };
