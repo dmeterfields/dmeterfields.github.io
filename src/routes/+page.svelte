a8ab974bbf4d0d8f692425c2c29b7db9d8313d78
Levi Tan Ong <levi.ong@gmail.com>
2024-11-07 22:07:28 +0800

    Add sigil, center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang='ts'>
+  import img from '$lib/assets/dmeterfields-sigil.svg';
+</script>
+
+<div class="container">
+  <img id="sigil" src="{img}" alt="Dmeter Fields Logo" />
+</div>
+
+<style>
+  :global(body) {
+    background-color: #005843;
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+  }
+
+  #sigil {
+    height: 32rem;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+</style>
