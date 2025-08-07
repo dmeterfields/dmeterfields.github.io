897c1598be9828f3af810e4decd5011402ad944d
Liv Tan Ong <livtanong@gmail.com>
2025-08-07 11:03:36 +0800

    Fully remove tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 // import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { sveltePhosphorOptimize } from 'phosphor-svelte/vite';
 
 export default defineConfig({
-    plugins: [tailwindcss(), sveltePhosphorOptimize(), sveltekit()],
+    plugins: [sveltePhosphorOptimize(), sveltekit()],
     test: { include: ['src/**/*.{test,spec}.{js,ts}'] }
 });
